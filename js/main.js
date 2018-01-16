631e6e3802a322b2d93c8a79d5ea1d5979e8a618
Gavin <luoxiangyu528@gmail.com>
2018-01-15 22:23:02 -0500

    Apply simplified chinese/traditional chinese convertion for the whole web page.

--- js/main.js
+++ js/main.js
@@ -4,12 +4,25 @@
 var main = function() {
 	var simpleTraditionalChineseConverter = new SimpleToTraditionalConverter(); 
 
+	var current_text_mode = 1 // simplified chinese characters.
+
 	var convertButtonClicked = function() {
 
-		var description_header = document.getElementById("description");
-		var str = description_header.innerHTML;
+		var all_dom_elements = document.querySelectorAll("p, h1, h2, h3, h4, li");
+		var convert_to = current_text_mode == 1?0:1;
 
-		description_header.innerHTML = simpleTraditionalChineseConverter.transformString(str, 0);
+		for (var i in all_dom_elements) {
+			dom_element = all_dom_elements[i];
+			var str = dom_element.innerHTML;
+			dom_element.innerHTML = simpleTraditionalChineseConverter.transformString(str, convert_to);
+		}
+
+		current_text_mode = convert_to;
+
+		// var description_header = document.getElementById("description");
+		// var str = description_header.innerHTML;
+		// 
+		// description_header.innerHTML = simpleTraditionalChineseConverter.transformString(str, 0);
 	}
 
 	var convert_button = document.getElementById("convert-button");
