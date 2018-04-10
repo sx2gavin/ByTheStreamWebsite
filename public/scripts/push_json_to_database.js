948043b75dbd890c09b90158de63eefb7fed50a0
Gavin <luoxiangyu528@gmail.com>
2018-04-09 23:24:51 -0400

    Connect this application with MongoDB Atlas online database.

--- public/scripts/push_json_to_database.js
+++ public/scripts/push_json_to_database.js
@@ -6,8 +6,8 @@
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-var url = "mongodb://127.0.0.1:27017/" // Remember to change this url for the production database.
+var url = "mongodb+srv://sx2gavin:glXishuipang@xishuipang-db-qo1sq.mongodb.net/"
+// var url = "mongodb://127.0.0.1:27017/" // Remember to change this url for the production database.
 var mongo = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
 var fs = require('fs');
@@ -28,68 +28,57 @@ else {
 
 var volumeDirectory = "../content/volume_" + volume;
 
-
-
 MongoClient.connect(url, function(err, db) {
 	if (err) {
 		throw err;
 	}
 	var xishuipangDb = db.db("Xishuipang");
-// 	xishuipangDb.collection("Articles").drop(function(err, delOK) {
-// 		if (err) {
-// 			console.log("Articles collection doesn't exist...");
-// 		}
-// 
-// 		if (delOK) {
-// 			console.log("Articles collection cleared...");
-// 		}
 
-		xishuipangDb.createCollection("Articles", function(err, res) {
+	xishuipangDb.createCollection("Articles", function(err, res) {
+		if (err) {
+			throw err;
+		}
+		console.log("Articles collection ready.");
+
+		// go to the volume folder and load each file.
+		fs.readdir(volumeDirectory, function(err, files) {
 			if (err) {
+				console.error("Error: " + volumeDirectory + " doesn't exist. Please make sure that the volume data is available.");
 				throw err;
 			}
-			console.log("Articles collection ready.");
+			console.log("In " + volumeDirectory + "...");
+			for (i in files) {
+				var filename = files[i];
 
-			// go to the volume folder and load each file.
-			fs.readdir(volumeDirectory, function(err, files) {
-				if (err) {
-					console.error("Error: " + volumeDirectory + " doesn't exist. Please make sure that the volume data is available.");
-					throw err;
+				// skip table_of_content file
+				if (filename == "table_of_content.json"){
+					continue;
 				}
-				console.log("In " + volumeDirectory + "...");
-				for (i in files) {
-					var filename = files[i];
+				if (path.extname(filename) == ".json") {
+					var fullFilePath = volumeDirectory + "/" + filename;
 
-					// skip table_of_content file
-					if (filename == "table_of_content.json"){
-						continue;
-					}
-					if (path.extname(filename) == ".json") {
-						var fullFilePath = volumeDirectory + "/" + filename;
+					// read the file and add it to the database.
+					fs.readFile(fullFilePath, function(err, data) {
+						if (err) {
+							console.error("Error: " + fullFilePath + " doesn't exist or cannot be read.");
+							throw err;
+						}
+						var jsonObj = JSON.parse(data);
+						var articleId = jsonObj.id;
+						var volumeNum = jsonObj.volume;
 
-						// read the file and add it to the database.
-						fs.readFile(fullFilePath, function(err, data) {
+						// either insert a new article or update the current one.
+						xishuipangDb.collection("Articles").updateOne({volume:volumeNum,id:articleId}, {$set: jsonObj}, {upsert: true}, function(err, res) {
 							if (err) {
-								console.error("Error: " + fullFilePath + " doesn't exist or cannot be read.");
 								throw err;
 							}
-							var jsonObj = JSON.parse(data);
-							var articleId = jsonObj.id;
-							var volumeNum = jsonObj.volume;
-
-							// either insert a new article or update the current one.
-							xishuipangDb.collection("Articles").updateOne({volume:volumeNum,id:articleId}, {$set: jsonObj}, {upsert: true}, function(err, res) {
-								if (err) {
-									throw err;
-								}
-								console.log(articleId + " added.");
-							});
+							console.log(articleId + " added.");
 						});
-					}
+					});
 				}
-			});
+			}
 		});
-// 	});
+	});
 
 	console.log("Xishuipang database connected!");
 });
--- routes.js
+++ routes.js
@@ -1,4 +1,13 @@
+
+var database_url = "mongodb+srv://sx2gavin:glXishuipang@xishuipang-db-qo1sq.mongodb.net/"
+// var database_url = "mongodb://127.0.0.1:27017/" // Remember to change this url for the production database.
+var mongo = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+var fs = require('fs');
+var path = require('path');
+
 var express = require('express');
+var url = require('url');
 var router = express.Router();
 
 var article = require('./controllers/article.js');
@@ -12,6 +21,25 @@ router.get('/volume', function(req, res, next) {
   res.render('volume');
 });
 
-router.get('/article/get', article.get);
+router.get('/article/get', function(req, res, next) {
+	var volumeId = url.parse(req.url, true).query.volume;
+
+	MongoClient.connect(database_url, function(err, db) {
+		if (err) {
+			throw err;
+		}
+		var articleCollection = db.db("Xishuipang").collection("Articles");
+
+		articleCollection.find({volume:volumeId}).toArray(function(err, result){
+			if (err) {
+				throw err;
+			}
+			res.json(result);
+			db.close();
+		});
+
+	});
+
+});
 
 module.exports = router;
