4ec4e852a61c233033c2efe0268356512423150b
Gavin <luoxiangyu528@gmail.com>
2018-08-09 22:30:17 -0400

    Add simplified and traditional chinese conversion at home page, add cookie to save user selection.

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,41 @@
 var main = function() {
 	getAllVolumesList();
+
+	var parameters = parseURL(window.location.href);
+
+    var characterVersion = "simplified";
+
+	if ("character" in parameters) {
+		characterVersion = parameters["character"];
+	} else {
+        characterVersion = getCookie("character");
+        if (characterVersion == "") {
+            characterVersion = "simplified";
+        }
+    }
+    setCookie("character", characterVersion, 365);
+
+	var invertCharacterVersion = "traditional";
+
+	if (characterVersion == "traditional") {
+		invertCharacterVersion = "simplified";
+	}
+
+    var characterTransformButton = document.getElementById(DOM_CHARACTER_SWITCH_BUTTON);
+    characterTransformButton.style.display = "block";
+    characterTransformButton.href = "?character=" + invertCharacterVersion;
+
+    
+    if (characterVersion == "traditional") {
+        var readButton = document.getElementById("read-button");
+        readButton.href = "volume?character=traditional";
+
+        var translator = new SimpleToTraditionalConverter;
+        var convertibleDOMs = document.getElementsByClassName("character-convertible");
+        for (var i = 0; i < convertibleDOMs.length; i++) {
+            convertibleDOMs[i].textContent = translator.transformString(convertibleDOMs[i].textContent, 0);
+        }
+    }
 }
 
 window.onload = main;
--- public/js/util.js
+++ public/js/util.js
@@ -76,3 +76,33 @@ var parseURL = function(url) {
 	}
 	return parameters;
 }
+
+var setCookie = function(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+var getCookie = function(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+var checkCookie = function() {
+    var user = getCookie("character");
+    if (user == "") {
+        user = "simplified";
+        setCookie("character", user, 365);
+    }
+}
