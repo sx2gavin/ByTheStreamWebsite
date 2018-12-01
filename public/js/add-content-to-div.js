d58edfe42958938146437279ec659f7d02b9a595
Gavin <luoxiangyu528@gmail.com>
2018-12-01 15:32:23 -0500

    Fix image sizes to float to either left or right.

--- public/js/add-content-to-div.js
+++ public/js/add-content-to-div.js
@@ -1,5 +1,7 @@
 var addContentToDiv = function(div, content, volumeNumber) {
 	// go through each line and check for <image.jpg> tags, replace it with a real <img> tag.
+
+    var floatLeft = true;
 	for (var index in content) {
 		var line = content[index].trim();
 		if (line.length == 0 || 
@@ -7,11 +9,12 @@ var addContentToDiv = function(div, content, volumeNumber) {
 			continue;
 		} else {
 			if (line[0] === '<' && line[line.length-1] === '>') {
-				var image_obj = createDOMElement('img', '', 'article-img card-img img-fluid rounded d-inline-block', '');
+                var floatStyleText = floatLeft?'float-left':'float-right';
+                image_obj = createDOMElement('img', '', 'article-img rounded d-inline ' + floatStyleText, '');
 				image_obj.src = "content/volume_" + volumeNumber + "/images/" + line.substring(1, line.length -1);
 				div.appendChild(image_obj);
-				// var article_text += "<img src=\"content/" + selected_volume + "/" + line.substring(1, line.length - 1) + "\" style=\"float:left; margin:10px;\" /><br />";
-			} else {
+                floatLeft = !floatLeft; // alternate image location from left to right to keep it interesting.
+            } else {
 				var p_obj = createDOMElement('p', '', 'card-text', line);
 				div.appendChild(p_obj);
 			}
