3964bfc6e3e2b786c094cd9833d26c2f5470ccff
Gavin <luoxiangyu528@gmail.com>
2019-08-30 23:53:51 -0400

    Add date of publish into each volume

--- public/scripts/push_json_to_database.js
+++ public/scripts/push_json_to_database.js
@@ -9,11 +9,11 @@
 // 
 // Use this script to upload content to MongoDB server.
 // 
-// <volume number> - 
+// <volume number>:  
 //      volume number indicates the content folder location. i.e. 50 indicates that
 //      the volume location is at ../content/volume_50/
 //
-// -t -
+// -t: 
 //      if -t flag is provided, then only the table of content json from the ../content/volume_<volume number> folder
 //      will be uploaded to the MongoDB server. This is useful if you just want to update some information
 //      in the table of content without having to reupload everything else again.
@@ -32,30 +32,29 @@ var numArgs = process.argv.length;
 // volume
 var volume = 0;
 var tableOfContentOnly = false;
+
 if (numArgs >= 3) {
 	volume = process.argv[2];
-    for (i in process.argv) {
-        var argv = process.argv[i]; 
-        if (argv === "--table-of-content-only" || argv == "-t") {
-            tableOfContentOnly = true;
-            console.log("Only uploading table of contents mode activated.");
-        }
-    }
+	for (i in process.argv) {
+		var argv = process.argv[i];
+		if (argv === "--table-of-content-only" || argv == "-t") {
+			tableOfContentOnly = true;
+			console.log("Only uploading table of contents mode activated.");
+		}
+	}
 } else {
 	console.error("Error: volume number is not provided in the command line.");
 	process.exit(1);
 }
 
-var volumeDirectory = "../content/volume_" + volume;
-
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, function (err, db) {
 	if (err) {
 		throw err;
 	}
 	var xishuipangDb = db.db("Xishuipang");
-
+	var volumeDirectory = "../content/volume_" + volume;
 	// go to the volume folder and load each file.
-	fs.readdir(volumeDirectory, function(err, files) {
+	fs.readdir(volumeDirectory, function (err, files) {
 		if (err) {
 			console.error("Error: " + volumeDirectory + " doesn't exist. Please make sure that the volume data is available.");
 			throw err;
@@ -70,8 +69,8 @@ MongoClient.connect(url, function(err, db) {
 
 			if (filename == "table_of_content_s.json" || filename == "table_of_content_t.json") {
 				// read the table of content file and add it to the database.
-				pending ++;
-				fs.readFile(fullFilePath, function(err, data) {
+				pending++;
+				fs.readFile(fullFilePath, function (err, data) {
 					if (err) {
 						console.error("Error: " + fullFilePath + " doesn't exist or cannot be read.");
 						throw err;
@@ -81,14 +80,13 @@ MongoClient.connect(url, function(err, db) {
 					var character = jsonObj.character;
 
 					// either insert a new article or update the current one.
-					xishuipangDb.collection("TableOfContents").updateOne({volume:volumeNum, character:character}, {$set: jsonObj}, {upsert: true}, function(err, res) {
+					xishuipangDb.collection("TableOfContents").updateOne({ volume: volumeNum, character: character }, { $set: jsonObj }, { upsert: true }, function (err, res) {
 						if (err) {
 							throw err;
 						}
 						console.log("Table of content added.");
 						pending--;
-						if (pending == 0 && finished == true)
-						{
+						if (pending == 0 && finished == true) {
 							db.close();
 						}
 					});
@@ -96,7 +94,7 @@ MongoClient.connect(url, function(err, db) {
 			} else if (path.extname(filename) == ".json" && tableOfContentOnly == false) {
 
 				// read the file and add it to the database.
-				fs.readFile(fullFilePath, function(err, data) {
+				fs.readFile(fullFilePath, function (err, data) {
 					if (err) {
 						console.error("Error: " + fullFilePath + " doesn't exist or cannot be read.");
 						throw err;
@@ -106,14 +104,13 @@ MongoClient.connect(url, function(err, db) {
 					var volumeNum = jsonObj.volume;
 
 					// either insert a new article or update the current one.
-					xishuipangDb.collection("Articles").updateOne({volume:volumeNum,id:articleId}, {$set: jsonObj}, {upsert: true}, function(err, res) {
+					xishuipangDb.collection("Articles").updateOne({ volume: volumeNum, id: articleId }, { $set: jsonObj }, { upsert: true }, function (err, res) {
 						if (err) {
 							throw err;
 						}
 						console.log(articleId + " added.");
 						pending--;
-						if (pending == 0 && finished == true)
-						{
+						if (pending == 0 && finished == true) {
 							db.close();
 						}
 					});
@@ -122,6 +119,5 @@ MongoClient.connect(url, function(err, db) {
 		}
 		finished = true;
 	});
-
 	console.log("Xishuipang database connected!");
 });
